7fca51bdc6514628298599e0d569fd42c103fca1
BuildTools <unconfigured@null.spigotmc.org>
2019-08-05 16:04:00 -0400

    hello world file

--- pythoncode.py
+++ pythoncode.py
@@ -4,3 +4,26 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+name = input("what is your name? ")  # type:
+
+print("hello " + name)
+
+x = 10
+print(x)
+
+x = int(input("enter a number."))
+if x < 10:
+    print("less than")
+elif x > 10:
+    print("greater than")
+
+x = int(input("enter a number."))
+if x % 2 == 0:
+    print("even")
+else:
+    print("number is odd")
+
+my_list = []
+for i in range(10):
+    my_list.append(i)
